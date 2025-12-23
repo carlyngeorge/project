88d91a307333ddf8a9db10aaa2caa11c14eb12ab
carlyngeorge <carlyngeorge06@gmail.com>
2025-12-23 19:58:15 +0400

    Create Expense class with attributes

---
+++ expense.py
@@ -0,0 +1,7 @@
+# expense.py
+class Expense:
+    def __init__(self, date, amount, category, description):
+        self.date = date
+        self.amount = amount
+        self.category = category
+        self.description = description
